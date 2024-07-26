6fef1903d49b83f3c84011030599511ad2bb46c0
MoloZzz <mega_oleksii@ukr.net>
2024-07-26 11:50:21 +0300

    feat/ add mongoose

--- server/library-system/src/app.module.ts
+++ server/library-system/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://megaoleksii5:hZMcn4sgv6aNeQwE@library.r3ucpy7.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
