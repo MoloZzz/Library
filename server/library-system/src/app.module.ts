a7bc24587ceb147323ba9159ad7fb186e149add0
MoloZzz <mega_oleksii@ukr.net>
2024-07-26 12:03:29 +0300

    feat/ add modules

--- server/library-system/src/app.module.ts
+++ server/library-system/src/app.module.ts
@@ -2,12 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { GenresModule } from './genres/genres.module';
+import { BooksModule } from './books/books.module';
+import { AdminModule } from './admin/admin.module';
+import { EmployeesModule } from './employees/employees.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://megaoleksii5:hZMcn4sgv6aNeQwE@library.r3ucpy7.mongodb.net/',
     ),
+    UsersModule,
+    GenresModule,
+    BooksModule,
+    AdminModule,
+    EmployeesModule,
+    TransactionsModule,
+    AuthenticationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
