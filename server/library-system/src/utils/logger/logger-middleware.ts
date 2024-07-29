c760c168578f883fe4ea75f52403af9671ffa1d1
MoloZzz <mega_oleksii@ukr.net>
2024-07-29 15:00:19 +0300

    style/yarn format

--- server/library-system/src/utils/logger/logger-middleware.ts
+++ server/library-system/src/utils/logger/logger-middleware.ts
@@ -3,17 +3,19 @@ import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    private readonly logger = new Logger('Request');
+  private readonly logger = new Logger('Request');
 
-    use(req: Request, res: Response, next: Function) {
-        const { method, originalUrl, body, query, params } = req;
-        const startTime = Date.now();
+  use(req: Request, res: Response, next: Function) {
+    const { method, originalUrl, body, query, params } = req;
+    const startTime = Date.now();
 
-        res.on('finish', () => {
-            const duration = Date.now() - startTime;
-            this.logger.log(`${method} ${originalUrl} - ${res.statusCode} [${duration}ms]`);
-        });
+    res.on('finish', () => {
+      const duration = Date.now() - startTime;
+      this.logger.log(
+        `${method} ${originalUrl} - ${res.statusCode} [${duration}ms]`,
+      );
+    });
 
-        next();
-    }
+    next();
+  }
 }
